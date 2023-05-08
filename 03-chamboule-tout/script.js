90e0f28a08a879b02b67b572563039c968b5d7ed
Benjamin Petermann <benjamin.petermann@gmx.at>
2023-05-08 21:16:45 +0200

    Add chamboule tout sketch

---
+++ 03-chamboule-tout/script.js
@@ -0,0 +1,240 @@
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import boxCoordinates from './boxCoordinates';
+import * as CANNON from 'cannon';
+import * as THREE from 'three';
+import * as dat from 'lil-gui';
+
+// Debug
+const gui = new dat.GUI();
+const guiParameters = {};
+guiParameters.positionX = 0;
+guiParameters.positionY = 0.5;
+guiParameters.force = 100;
+guiParameters.shoot = () => {
+  shoot();
+};
+
+gui.add(guiParameters, 'positionX').min(-4).max(4).step(0.5).name('position X');
+gui
+  .add(guiParameters, 'positionY')
+  .min(0.5)
+  .max(4)
+  .step(0.5)
+  .name('position Y');
+gui.add(guiParameters, 'force').min(100).max(5000).step(10).name('force');
+gui.add(guiParameters, 'shoot');
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new THREE.Scene();
+
+// Physics
+const world = new CANNON.World();
+world.broadphase = new CANNON.SAPBroadphase(world);
+world.allowSleep = true;
+world.gravity.set(0, -9.82, 0);
+
+// Default material
+const defaultMaterial = new CANNON.Material('default');
+const defaultContactMaterial = new CANNON.ContactMaterial(
+  defaultMaterial,
+  defaultMaterial,
+  {
+    friction: 0.1,
+    restitution: 0.7,
+  }
+);
+world.defaultContactMaterial = defaultContactMaterial;
+
+// Floor
+const floorShape = new CANNON.Plane();
+const floorBody = new CANNON.Body();
+floorBody.mass = 0;
+floorBody.addShape(floorShape);
+floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5);
+world.addBody(floorBody);
+
+// Utils
+const objectsToUpdate = [];
+
+// Create sphere
+const sphereGeometry = new THREE.SphereGeometry(1, 20, 20);
+const sphereMaterial = new THREE.MeshStandardMaterial({
+  metalness: 0.3,
+  roughness: 0.4,
+});
+
+const shoot = () => {
+  const mesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  mesh.castShadow = true;
+  mesh.scale.set(0.5, 0.5, 0.5);
+  mesh.position.copy({ x: 0, y: 1, z: 5 });
+  scene.add(mesh);
+
+  // Cannon.js body
+  const shape = new CANNON.Sphere(0.5, 0.5, 0.5);
+
+  const body = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 0, 0),
+    shape: shape,
+    material: defaultMaterial,
+  });
+  body.position.copy({
+    x: guiParameters.positionX,
+    y: guiParameters.positionY,
+    z: 5,
+  });
+  body.applyLocalForce(
+    new CANNON.Vec3(0, 200, -guiParameters.force),
+    new CANNON.Vec3(0, 0, 0)
+  );
+
+  world.addBody(body);
+
+  objectsToUpdate.push({ mesh, body });
+};
+
+// Create box
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshStandardMaterial({
+  metalness: 0.3,
+  roughness: 0.4,
+  envMapIntensity: 0.5,
+});
+
+const createBox = (position) => {
+  const mesh = new THREE.Mesh(boxGeometry, boxMaterial);
+  mesh.scale.set(1, 1, 1);
+  mesh.castShadow = true;
+  mesh.position.copy(position);
+  scene.add(mesh);
+
+  // Cannon.js body
+  const shape = new CANNON.Box(new CANNON.Vec3(1 * 0.5, 1 * 0.5, 1 * 0.5));
+
+  const body = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 0, 0),
+    shape: shape,
+    material: defaultMaterial,
+  });
+  body.position.copy(position);
+  world.addBody(body);
+
+  objectsToUpdate.push({ mesh, body });
+};
+
+// Reset
+guiParameters.start = () => {
+  for (const object of objectsToUpdate) {
+    world.removeBody(object.body);
+    scene.remove(object.mesh);
+  }
+
+  objectsToUpdate.splice(0, objectsToUpdate.length);
+
+  for (const box of boxCoordinates) {
+    createBox({
+      x: box.x,
+      y: box.y,
+      z: box.z,
+    });
+  }
+};
+gui.add(guiParameters, 'start');
+guiParameters.start();
+
+// Floor
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshStandardMaterial({
+    color: '#777777',
+    metalness: 0.3,
+    roughness: 0.4,
+  })
+);
+floor.receiveShadow = true;
+floor.rotation.x = -Math.PI * 0.5;
+scene.add(floor);
+
+// Lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.camera.left = -7;
+directionalLight.shadow.camera.top = 7;
+directionalLight.shadow.camera.right = 7;
+directionalLight.shadow.camera.bottom = -7;
+directionalLight.position.set(5, 5, 5);
+scene.add(directionalLight);
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(-6, 6, 6);
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Animate
+const clock = new THREE.Clock();
+let oldElapsedTime = 0;
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - oldElapsedTime;
+  oldElapsedTime = elapsedTime;
+
+  // Update physics
+  world.step(1 / 60, deltaTime, 3);
+
+  for (const object of objectsToUpdate) {
+    object.mesh.position.copy(object.body.position);
+    object.mesh.quaternion.copy(object.body.quaternion);
+  }
+
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+
+tick();
