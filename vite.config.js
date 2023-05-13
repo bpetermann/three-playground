dd208fa654c1897a3edab30ec22cab15606dd676
Benjamin Petermann <benjamin.petermann@gmx.at>
2023-05-13 16:11:49 +0200

    Change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,28 @@
+import { resolve } from 'path';
 import glsl from 'vite-plugin-glsl';
 
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist');
+
 export default {
+  root: 'src/',
+  publicDir: '../public/',
+  base: './',
+  build: {
+    outDir,
+    emptyOutDir: true,
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        '01': resolve(root, '01', 'index.html'),
+        '02': resolve(root, '02', 'index.html'),
+        '03': resolve(root, '03', 'index.html'),
+        '04': resolve(root, '04', 'index.html'),
+        '05': resolve(root, '05', 'index.html'),
+
+      },
+    },
+  },
   plugins: [glsl()],
 };
